7efeeac678e454e37942246269110cc7113daf81
merrinmariyajaison <143310494+merrinmariyajaison@users.noreply.github.com>
2024-02-25 23:40:52 +0530

    Add files via upload

---
+++ Method_overridingws.java
@@ -0,0 +1,51 @@
+//super object is used to overcome overriding
+import java.io.*;
+class Person{
+int age;
+String name;
+public void read( ){
+DataInputStream din = new DataInputStream(System.in);
+try{
+System.out.println("Enter the name and age");
+name = din.readLine();
+age = Integer.parseInt(din.readLine());
+}
+catch(Exception e){
+}
+}
+public void disp()
+{
+System.out.println("Name : "+name +"  Age :"+age);
+}
+}
+class Teacher extends Person{
+String Subj;
+public void read(){
+super.read();
+DataInputStream din = new DataInputStream(System.in);
+try{
+System.out.println("Enter the subject");
+Subj = din.readLine();
+}
+catch(Exception e){
+
+}
+}
+public void disp(){
+super.disp();
+System.out.println("Subject "+Subj);
+}
+}
+public class Method_overridingws{
+public static void main(String args[ ])
+{
+  Person p;
+ Teacher t;
+p= new Person();
+t=new  Teacher();
+p.read();
+t.read();
+p.disp();
+t.disp();
+}
+}
