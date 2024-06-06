6a26e67616684d1199724e829bed4357668bbf84
merrinmariyajaison <143310494+merrinmariyajaison@users.noreply.github.com>
2024-06-06 21:28:37 +0530

    Add files via upload

---
+++ P23.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+class EvenNumDisp {
+	public static void evenPrint(int num) {
+		for(int i=0; i<=num; i=i+2){
+			System.out.print(i+" ");
+		}
+	}
+	
+}
+
+class P23 {
+	public static void main(String args[]) {
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int num = s.nextInt();
+		System.out.print("Even numbers are: \n");
+		EvenNumDisp.evenPrint(num);
+	}
+}
